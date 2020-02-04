c1237a0b5258d6fef29f66dcab802106df5cbf27
e165714 <e165714@ie.u-ryukyu.ac.jp>
2020-02-04 20:49:17 +0900

    rails側での認証の動きを修正(脳死でインスタンス変数@に変更した)

--- rails/app/controllers/sessions_make_controller.rb
+++ rails/app/controllers/sessions_make_controller.rb
@@ -3,13 +3,13 @@ class SessionsMakeController < ApplicationController
     #redirect_to login_url
   end
   def create
-    user = User.find_by(name: params[:session][:name].downcase)
+    @user = User.find_by(name: params[:session][:name].downcase)
     #debugger
-    if user.login_status == 1
-      user.login_status = 0
-      user.save
-      session[:user_id] = user.id
-      redirect_to user_url(user)
+    if @user.login_status == 1
+      @user.login_status = 0
+      @user.save
+      session[:user_id] = @user.id
+      redirect_to user_url(@user)
     else 
       render 'new'
     end
