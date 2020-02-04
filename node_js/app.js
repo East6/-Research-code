22efed7504254f34a4306066e884d9114631e047
e165714 <e165714@ie.u-ryukyu.ac.jp>
2020-02-04 20:26:48 +0900

    node.js側の認証の動きを修正。(rails側での認証の動きがおかしい)

--- node_js/app.js
+++ node_js/app.js
@@ -28,6 +28,7 @@ function handler (req, res) {
 
 // socket関数?が繋がった時に処理をする??
 io.on('connection', function (socket) {
+  
   // クライアントへデータ送信
   //socket.emit('news', { hello: 'world' });
   // クライアントから秘密鍵のデータを受信
@@ -35,29 +36,62 @@ io.on('connection', function (socket) {
     privatekey_data = data;
     console.log(privatekey_data);
     console.log("-----------------------------------ユーザ名を送ったけど,取れるかな-------------------")
-    // ssh認証を行う
+
+
+    //エラーで,サーバが落ちないようにする
+    //ssh2認証で失敗したときに,ここでエラーが出る --> 失敗したと,ユーザに通知する
+    process.on('uncaughtException', function(err) {
+      console.log(err);
+      //　認証失敗したと,ユーザsocket通信を行う
+      socket.emit('SSH Sertification status', { SSH_Sertification_status: false});
+    });
     try{
-      SSH_Sertification();
-      //認証が成功した時(エラーが出ない時)にここが実行される
-      console.log("認証okです")
-      //rails側のユーザ(今の所 higashi)に対して，login_session を1にするコードを記述した
-      const promise = POST_method();
-      promise.then(function(){
-        console.log("postは成功したはず")
-      }).catch(function(error){
-        console.log("----------------postは失敗したっぽい--------------------")
-        console.log(error)
-      });
-      //socket.ioで成功したという信号を送る(ユーザ名も)
-      socket.emit('SSH Sertification status', { SSH_Sertification_status: true , name: "higashi"}); 
+      //ユーザが秘密鍵を選択したとき,ここの中が実行される
+
+      // ssh認証を行う
+      //ssh2接続を試みる  始まり-----------------------------
+      var conn = new Client();
+      conn.on('ready', function() {
+        console.log('Client :: ready');
+        conn.shell(function(err, stream) {
+          if (err) throw err;
+          stream.on('close', function() {
+            console.log('Stream :: close');
+            conn.end();
+          }).on('data', function(data) {
+            console.log('OUTPUT: ' + data);
+          });
+
+          //rails側のユーザ(今の所 higashi)に対して，login_session を1にするコードを記述した
+          const promise = POST_method();
+          promise.then(function(){
+            console.log("postは成功したはず")
+          }).catch(function(error){
+            console.log("----------------postは失敗したっぽい--------------------")
+            console.log(error)
+          });
+          //socket.ioで成功したという信号を送る(ユーザ名も)
+          socket.emit('SSH Sertification status', { SSH_Sertification_status: true , name: privatekey_data['name']}); 
+
+          stream.end('ls -l\nexit\n');
+        });
+      }).connect({
+        host: '10.0.2.53',
+        port: 22,
+        username: privatekey_data['name'],
+        privateKey: privatekey_data['PrivateKey']
+      });  
+      //ssh2接続を試みる  終わり-----------------------------
+
     } catch(e){
-      //認証エラーが出た時の処理
+      //秘密鍵を指定していないときに,ここの中が実行される
+
       console.log("おそらく認証のエラーが出ました。------------------")
       console.log(e)
       console.log("おそらく認証のエラーが出ました。------------------")
-      //認証エラーしたときにここが実行される
+      //認証エラーしたときにここが実行される(秘密鍵ファイルでないとき)
       ////socket.ioで失敗したという信号を送る
-      socket.emit('SSH Sertification status', { SSH_Sertification_status: false });
+      socket.emit('SSH Sertification status', { SSH_Sertification_status: "no-privatekey"});
       console.log("socke.emitをしたよ")
     }
   });
